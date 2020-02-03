de903d4bf809d3337b0f50b73903a4e2ce854fe8
Christian <mail@christianschmidt.info>
2020-02-03 22:43:24 +0100

    switched to Vue <3

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,12 +1,40 @@
-var app = angular.module("miniblog", []);
+// var app = angular.module("miniblog", []);
+// app.controller("PostsController", function($scope, $http) {
+//   $http.get('https://chrisschmi.github.io/scripts/posts.json').
+//     success(function(data, status, headers, config) {
+//       //console.log(data);
+// 	    $scope.posts = data;
+//     }).
+//     error(function(data, status, headers, config) {
+//       // log error
+//     });
+// });
 
-app.controller("PostsController", function($scope, $http) {
-  $http.get('https://chrisschmi.github.io/scripts/posts.json').
-    success(function(data, status, headers, config) {
-      //console.log(data);
-	    $scope.posts = data;
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
-});
+const app = new Vue({
+    el: '#app',
+    data: {
+      search: '',
+      loading: false,
+      posts: []
+    },
+    computed: { },
+    methods: { },
+    mounted: function()
+    {
+      var self = this;
+      self.loading = true;
+      var link = 'https://chrisschmi.github.io/scripts/posts.json';
+
+      axios.get(link)
+      .then(response => {
+        self.posts = response.data;
+        self.loading = false;
+      })
+      .catch(error => 
+        {
+          console.log(error);
+          self.loading = false;
+        }
+      )
+    }
+  });
