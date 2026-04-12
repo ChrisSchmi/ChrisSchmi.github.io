5740cd98cb449272cda695611048286e257d9ea0
Christian <mail@christianschmidt.info>
2026-04-12 17:13:51 +0200

    refactored service worker

--- sw.js
+++ sw.js
@@ -1,40 +1,93 @@
 /*  Service Worker Experiment */
 /*  Inspired by: https://www.google.de/_/chrome/newtab-serviceworker.js */
 /*  Inspired by: https://www.sblum.de/progressive-web-apps  */
-var FILES = [
-  'manifest.json'
-];
-
-var BLACKLIST = [];
-var CACHE_NAME = '20240423';
+const CACHE_NAME = '20260412';
 const OFFLINE_URL = '/offline.html';
 
-self.addEventListener('install',
-    (event) => {
+const FILES_TO_CACHE = [
+  OFFLINE_URL,
+  '/manifest.json'
+];
 
-        // Cache the offline page when installing the service worker
-        event.waitUntil(
-            fetch(OFFLINE_URL, { credentials: 'include' })
-                .then(response => caches.open(CACHE_NAME).then(cache => {
-                    cache.put(OFFLINE_URL, response);
-                    cache.addAll(FILES);
-                }
-                ))
-        );
-    });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => {
+      if ('navigationPreload' in self.registration) {
+        return self.registration.navigationPreload.enable();
+      }
+    })
+  );
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', (event) => {
+  const request = event.request;
 
-    if (event.request.mode === 'navigate') {
-        return event.respondWith(fetch(event.request).catch(() => caches.match(OFFLINE_URL)));
-    }
+  if (request.method !== 'GET') {
+    return;
+  }
 
-    // Passthrough for everything else
+  if (request.mode === 'navigate') {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        }).catch(ex => {
-            console.log(ex);
-        })
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          return await fetch(request);
+        } catch (error) {
+          const cache = await caches.open(CACHE_NAME);
+          const offlineResponse = await cache.match(OFFLINE_URL);
+          return offlineResponse;
+        }
+      })()
     );
+    return;
+  }
+
+  if (request.destination === 'image') {
+    event.respondWith(
+      (async () => {
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        try {
+          const networkResponse = await fetch(request);
+          const cache = await caches.open(CACHE_NAME);
+          cache.put(request, networkResponse.clone());
+          return networkResponse;
+        } catch (error) {
+          return new Response('', { status: 503, statusText: 'Service Unavailable' });
+        }
+      })()
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => {
+      return cachedResponse || fetch(request);
+    })
+  );
 });
